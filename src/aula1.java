f68a6392fb7eb9a19c17353b0583fc74af5cb7a8
lucascardoso701@outlook.com <ifnmg@192.168.163.13>
2019-03-19 20:09:57 -0300

    alteração inicial

--- src/aula1.java
+++ src/aula1.java
@@ -1,9 +1,11 @@
 
+
+
+
 public class aula1 {
       public static void main(String[] args){
       
-      
-     
-
+      int a= 100;
+     System.out.println ("nfhn");
               
              }  }
